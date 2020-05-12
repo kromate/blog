a72219305b5a45bbe523c181b790c316529ea6b0
kromate <akpananthony33@gmail.com>
2020-05-12 12:08:46 +0100

    initial.2

---
+++ js/default.js
@@ -0,0 +1,15 @@
+
+    /*====================================================
+      THEME SETTINGS & GLOBAL VARIABLES
+    ====================================================*/
+    //  1. Disqus comment settings
+  var disqus_shortname = 'avantghosttheme'; // required: replace example with your forum shortname
+
+  //  2. Fixed navbar
+  var fixed_navbar = true;
+
+  //  3. Dark Color Scheme
+  var dark_color_scheme = false;
+
+  // 4. Content API key ( it required to search work)
+  var api_key = '3cc880da1ffd75e32f3abb8545';
---
+++ js/default2.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/avant.gbjsolution.com\/birds-making-their-nest\/","comments":0},{"id":"http:\/\/avant.gbjsolution.com\/explore-the-new-asia\/","comments":6},{"id":"http:\/\/avant.gbjsolution.com\/fresh-fashion-of-summer-20015\/","comments":6},{"id":"http:\/\/avant.gbjsolution.com\/what-we-use-to-maintain-schedule\/","comments":0},{"id":"http:\/\/avant.gbjsolution.com\/benefits-of-rose\/","comments":1},{"id":"http:\/\/avant.gbjsolution.com\/bike-is-my-passion\/","comments":0}]});
+}
